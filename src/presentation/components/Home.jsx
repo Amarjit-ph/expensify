16ea44e8cd5f834dfd0b30ade946b9fbc6772695
Amarjit Pheiroijam <Amarjitp9@gmail.com>
2022-03-29 13:45:28 +0530

    Improvements

--- src/presentation/components/Home.jsx
+++ src/presentation/components/Home.jsx
@@ -100,7 +100,6 @@ const Home = ({expenseCount,expensesTotal,expensesMonthlyTotal,expensesYearlyTot
                                         
                                         <img src={url} className='inline object-cover w-12 h-12 mr-2 rounded-full' alt="" />
                                        
-                                         
                                         <p class="text-sm text-gray-700 dark:text-white ml-2 font-semibold border-b border-gray-200">
                                         Total spending of this year<br/>
                                         
@@ -114,7 +113,8 @@ const Home = ({expenseCount,expensesTotal,expensesMonthlyTotal,expensesYearlyTot
                                     </div>
                                 </div>
                                 <div class="w-full h-3 bg-gray-100">
-                                    {visualSpending(expensesTotal)}
+                                     <div class="w-full h-full text-center text-xs text-white bg-green-400"></div>
+                                    {/* {visualSpending(expensesTotal)} */}
                                     {/* <div class="w-full h-full text-center text-xs text-white bg-green-400"></div> */}
                                 </div>
                             </div>
@@ -164,6 +164,27 @@ const Home = ({expenseCount,expensesTotal,expensesMonthlyTotal,expensesYearlyTot
                 </div>
 
 
+
+                {/* NOTICE */}
+                <div class="bg-orange-200 px-6 py-4 my-4 rounded-md text-sm flex items-center w-3/4 xl:w-2/4 bg-white">
+                <svg
+                    viewBox="0 0 24 24"
+                    class="text-yellow-600 w-5 h-5 sm:w-5 sm:h-5 mr-3"
+                    >
+                    <path
+                        fill="currentColor"
+                        d="M23.119,20,13.772,2.15h0a2,2,0,0,0-3.543,0L.881,20a2,2,0,0,0,1.772,2.928H21.347A2,2,0,0,0,23.119,20ZM11,8.423a1,1,0,0,1,2,0v6a1,1,0,1,1-2,0Zm1.05,11.51h-.028a1.528,1.528,0,0,1-1.522-1.47,1.476,1.476,0,0,1,1.448-1.53h.028A1.527,1.527,0,0,1,13.5,18.4,1.475,1.475,0,0,1,12.05,19.933Z"
+                        ></path>
+                </svg>
+                <span class="text-yellow-800">
+                    Comming soon ! Expenses Analytics is under development please ignore this section.
+                </span>
+                </div>
+                 
+
+                
+
+                {/* ANALYTICS SECTION */}
                 <div class="grid grid-cols-1 text-black md:grid-cols-2 lg:grid-cols-3 gap-4 my-4">
                     <div class="w-full">
                         <div class="shadow-lg px-4 py-6 w-full bg-white dark:bg-gray-700 relative">
